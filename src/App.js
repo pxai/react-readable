487690ce13e0bb5e02d1227e8f4d6bc32d771f1e
pxai <p@pello.io>
2017-09-08 16:24:10 -0400

    Routes in place!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <div className="list-books">
+            <div className="list-books-title">
+              <h1>Pello's MyReads</h1>
+            </div>
+
+        <Route path="/" exact render={() => (
+            <h1>Home</h1>
+        )}/>
+        <Route path="/posts" exact render={({history}) => (
+          <h1>Posts</h1>
+        )}/>
+        <Route path='/comments' render={({ history }) => (
+          <h1>Comments</h1>
+        )}/>
+        <div className="open-search">
+              <Link to='/' className='open-search'>Home</Link>
+            </div>
+            <div className="open-search">
+              <Link to='/posts' className='open-search'>Posts</Link>
+            </div>
+            <div className="open-create">
+              <Link to='/comments' className='add-book'>Comments</Link>
+            </div>
+      </div>
       </div>
     );
   }
